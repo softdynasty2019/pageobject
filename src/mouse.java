d1310076863db91c27f1b2c9f7f25478aabfac13
SoftDynasty <softdynasty2019@gmail.com>
2023-05-23 18:57:47 -0400

    hell

--- src/mouse.java
+++ src/mouse.java
@@ -28,7 +28,6 @@ public class mouse {
 		a.moveToElement(driver.findElement(By.id("twotabsearchtextbox"))).contextClick().build().perform();
 		
 		//Employee from Bangladesh
-		//Update from india 
 		
 
 	}
